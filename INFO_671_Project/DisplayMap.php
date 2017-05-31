c4982a66b340ddbcfe848dfc12205ec5d9727b1d
Robert Cheng <chengchang8@gmail.com>
2017-05-31 17:08:55 -0400

    pins dropped

--- INFO_671_Project/DisplayMap.php
+++ INFO_671_Project/DisplayMap.php
@@ -8,6 +8,18 @@
         
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     </head>
+    
+    <style>
+        #map {
+          height: 100%;
+        }
+        
+        html, body {
+          height: 100%;
+          margin: 0;
+          padding: 0;
+        }
+    </style>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
 
@@ -15,7 +27,38 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" >
 
     <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" >
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
+    
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCVBCYmr5045H4rYmjaKybJBcg10_vmVHc&libraries=places"></script>
+    <script>
+        var map;    
+        var image = {
+            url: 'http://www.iconsdb.com/icons/preview/royal-blue/map-marker-2-xxl.png',
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(40, 40)
+        };
+        function initMap() {
+          var pyrmont = {lat: 39.9537552, lng: -75.1876767};
+          var uluru = {lat: 39.9568713, lng: -75.1837215};
+          map = new google.maps.Map(document.getElementById('map'), {
+            center: pyrmont,
+            zoom: 17
+          });
+
+         var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+         });
+         
+         var current = new google.maps.Marker({
+          position: pyrmont,
+          map: map,
+          title:"Current Location",
+          icon:image
+         });
+        }        
     </script>
     
     <body>
@@ -32,8 +75,10 @@
                 </ul>
             </div>
         </nav>
+        <div id="map"></div>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCVBCYmr5045H4rYmjaKybJBcg10_vmVHc&libraries=places&callback=initMap" async defer></script>
+<!--            <div>
 
-            <div>
                 <iframe 
                     src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d12232.96504960305!2d-75.1953933!3d39.9583583!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1495660609021" 
                     width="1350" 
@@ -42,7 +87,7 @@
                     style="border:0" 
                     allowfullscreen>
                 </iframe>
-            </div>
+            </div>-->
 
     </body>
 <?php
